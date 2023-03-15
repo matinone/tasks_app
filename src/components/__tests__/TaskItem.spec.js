e502f43907b5ad1eb5d6b9ef8cb55a8e01ca7050
mbrignone <mnbrignone@gmail.com>
2023-03-15 12:48:28 -0300

    Add testcase for marking a task as done

--- src/components/__tests__/TaskItem.spec.js
+++ src/components/__tests__/TaskItem.spec.js
@@ -1,25 +1,39 @@
-import { it, expect, describe, beforeEach } from "vitest";
+import { it, expect, describe, vi } from "vitest";
 import { shallowMount } from "@vue/test-utils";
-import { setActivePinia, createPinia } from "pinia";
+import { createTestingPinia } from "@pinia/testing";
+
+import useTaskStore from "@/stores/taskStore";
 
 import TaskItem from "@/components/TaskItem.vue";
 import EditTaskForm from "@/components/EditTaskForm.vue";
 
-describe("TaskItem.vue", () => {
-  beforeEach(() => {
-    setActivePinia(createPinia());
+const createTaskItemComponent = () => {
+  const task = {
+    title: "Task Title",
+    description: "Task Description",
+    done: false
+  };
+
+  const component = shallowMount(TaskItem, {
+    props: { task },
+    global: {
+      plugins: [
+        createTestingPinia({
+          createSpy: vi.fn,
+          initialState: {
+            tasks: { tasks: [task] }
+          }
+        })
+      ]
+    }
   });
 
-  it("Render single task", () => {
-    const task = {
-      title: "Task Title",
-      description: "Task Description",
-      done: false
-    };
+  return { task, component };
+};
 
-    const component = shallowMount(TaskItem, {
-      props: { task }
-    });
+describe("TaskItem.vue", () => {
+  it("Render single task", async () => {
+    const { component } = createTaskItemComponent();
 
     expect(component.text()).toContain("Task Title");
     expect(component.text()).toContain("Task Description");
@@ -30,5 +44,20 @@ describe("TaskItem.vue", () => {
 
     const editForm = component.findAllComponents(EditTaskForm);
     expect(editForm).toHaveLength(0);
+  });
+
+  it("Mark task as done", async () => {
+    const { task, component } = createTaskItemComponent();
+
+    const taskStore = useTaskStore();
+    taskStore.resolveTask = vi.fn((task) => {
+      task.done = true;
+    });
+
+    // click "Mark as DONE" button
+    let buttons = component.findAll("button");
+    await buttons[1].trigger("click");
+    expect(taskStore.resolveTask).toHaveBeenCalledWith(task);
+    expect(component.text()).not.toContain("Mark as DONE");
   });
 });
